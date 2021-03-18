e05f0d11fe58f91599a93c3c87a4cf903912a630
Nicole Zhang <nicole@Nicoles-MacBook-Pro.local>
2021-03-18 11:22:03 +0800

    - fix M1 compatibility

--- Formula/tezos-client.rb
+++ Formula/tezos-client.rb
@@ -35,7 +35,7 @@ class TezosClient < Formula
   def make_deps
     ENV.deparallelize
     ENV["CARGO_HOME"]="./.cargo"
-    system "rustup-init", "--default-toolchain", "1.44.0", "-y"
+    system "rustup-init", "--default-toolchain", "1.50.0", "-y"
     system "opam", "init", "--bare", "--debug", "--auto-setup", "--disable-sandboxing"
     system ["source .cargo/env",  "make build-deps"].join(" && ")
   end
